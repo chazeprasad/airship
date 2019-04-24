aa610fc0d1e796cd28e687ae74405f01b176da9e
Prasad S <prasad@Prasads-MacBook-Pro.local>
2019-04-24 23:05:22 +0530

    updates lib

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,5 +3,6 @@ import { Api } from "./core/ApiDecorator";
 import { Paper } from "./core/Paper";
 import { Head, Options, Get, Post, Put, Patch, Delete, Del, All } from "./core/RouteDecorator";
 import { Status } from "./core/Status";
+import { ApiError, ApplicationError } from "./error";
 import { Airship } from './airship';
-export { Head, Options, Get, Post, Put, Patch, Delete, Del, All, Api, ActionController, Paper, Status, Airship };
+export { Head, Options, Get, Post, Put, Patch, Delete, Del, All, Api, ActionController, Paper, Status, Airship, ApiError, ApplicationError };
